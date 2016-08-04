dc89eaabf2fc7865151697b45b124682bc10f710
mursic <mursic@gmail.com>
2016-08-04 16:53:02 -0700

    UI changed to include ingredients and recomposing

--- UI/app/models.py
+++ UI/app/models.py
@@ -4,7 +4,9 @@ from app import db
 class Poem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(128), index=True, unique=True)
+    segm_txt = db.Column(db.String(1000), index=True, unique=False)
     poem_txt = db.Column(db.String(400), index=True, unique=False)
+    theme_txt = db.Column(db.String(400), index=True, unique=False)
     lastrating = db.Column(db.Integer, index=True, unique=False)
     meanrating = db.Column(db.Float, index=True, unique=False)
     votes = db.Column(db.Integer, index=True, unique=False)
